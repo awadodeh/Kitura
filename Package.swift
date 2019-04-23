7a1b0ab10f8c51604ec7e4fe4ef2dc1cb5d15afa
owdaa <awadodeh@gmail.com>
2019-04-23 10:19:50 -0500

    first commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KituraTIL",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/IBM-Swift/Kitura.git",
+                 .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git",
+                 .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",
+                 .upToNextMajor(from: "3.0.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "KituraTIL",
+                dependencies: ["Kitura" , "HeliumLogger", "CouchDB"],
+                path: "Sources")
+    ]
+)
